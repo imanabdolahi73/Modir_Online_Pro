165cb5503a509ede4effaeadb37b18926b2e8caa
iman abdolahi <abdolahi.iman73@gmail.com>
2023-05-10 03:34:29 +0430

    Merge Complited And Bug Fixed

--- EndPoint.Site/Controllers/InOutMaterialController.cs
+++ EndPoint.Site/Controllers/InOutMaterialController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ModirOnline.Application.Services.Material.Commands;
+using ModirOnline.Application.Services.Material.Queries;
+using ModirOnline.Application.Services.Product.Commands;
 
 namespace EndPoint.Site.Controllers
 {
     public class InOutMaterialController : Controller
     {
-        public IActionResult Index()
+        private readonly IInOutMaterialService _inOutMaterialService;
+        private readonly IMaterialManagmentService _materialManagmentService;
+        private readonly IGetAllMaterialCategoriesService _getAllMaterialCategoriesService;
+        private readonly IGetAllInventoriesService _getAllInventoriesService;
+        public InOutMaterialController(
+            IInOutMaterialService inOutMaterialService,
+            IMaterialManagmentService materialManagmentService,
+            IGetAllMaterialCategoriesService getAllMaterialCategoriesService,
+            IGetAllInventoriesService getAllInventoriesService)
         {
-            return View();
+            _inOutMaterialService = inOutMaterialService;
+            _materialManagmentService = materialManagmentService;
+            _getAllMaterialCategoriesService = getAllMaterialCategoriesService;
+            _getAllInventoriesService = getAllInventoriesService;
         }
+        public IActionResult Index(int? MaterialCategoryID)
+        {
+            ViewBag.Categories = new SelectList(_getAllMaterialCategoriesService.Execute().Data, "Id", "Name");
+            ViewBag.Inventories = new SelectList(_getAllInventoriesService.Execute().Data, "Id", "Name");
+
+            return View(_materialManagmentService.Get_Materials(MaterialCategoryID));
+        }
+
+        [HttpPost]
+        public IActionResult Add(Request_InOut_Material_Dto request)
+        {
+            request.EmployeeID = 1;
+            return Json(_inOutMaterialService.Add(request));
+        }
+
+
     }
 }
