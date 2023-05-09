e4cd138754ec4428d802fbd56d2229b2226d5af8
iman abdolahi <abdolahi.iman73@gmail.com>
2023-05-10 03:00:58 +0430

    Start Fix Bug

---
+++ EndPoint.Site/Controllers/InOutMaterialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.Controllers
+{
+    public class InOutMaterialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
